b45b38800ed61131e3e8df2af513d2073b1038cc
tatsuya-kusumoto <tkusumot118217@gmail.com>
2024-04-07 20:19:24 +0900

    Lesson7 課題提出

---
+++ src/animal/Human.java
@@ -0,0 +1,24 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+
+    private String hobby;
+
+    @Override
+    public void think() {
+        System.out.println("私は " + hobby + " について考えています。");
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Human(String name,int age,String hobby) {
+        super(name,age);
+        this.hobby = hobby;
+    }
+}
